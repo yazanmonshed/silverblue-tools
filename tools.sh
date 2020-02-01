ac2f2df7d9367f5e0d850b2aa7d4d42053993026
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-01 17:16:24 +0200

    fixed file and add more tools

--- tools.sh
+++ tools.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
-#Author: yazan al monshed
-#Date 21 / 1 / 2020
-# script for install tools working from commans line for fedora sliveblue
+# Author: yazan al monshed
+# Date 21 / 1 / 2020
+# script for install tools working from command line for fedora sliveblue
+# vim : editor works in command line
+# task : is todo list work from command line
+# vmstat : show the state of system and performanice 
+# gcc : is compiler for C/C++
+# neofetch : is fast highly customizble system info script
+# nl : funy commad line 
 
-
-# vim editor works in command line
-# task is todo list work from command line
-# vmstat show the state of system and performanice 
-# gcc is compiler for C/C++
-# neofetch is fast highly customizble system info script
-
-read -p "what is your distro?" distroname
-
-case $distroname in 
-	"fedora")
-sudo rpm-ostree install vim task  vmstat gcc neofetch 
+read -p "type name tool to install it " tool
+case $tool in 
+"vim")
+sudo rpm-ostree install vim
 ;;
-	"debian")
-sudo apt-get install vim task vmstat gcc neofetch
+"nano")
+sudo rpm-ostree install nano
 ;;
- 	*)
-
-echo "sorry, your distro name is not found"
+"task")
+sudo rpm-ostree install task
 ;;
-
+"podman")
+sudo rpm-ostree install podman
+;;
+"gcc")
+sudo rpm-ostree install gcc 
+;;
+"vmstat")
+sudo rpm-ostree install vmstat 
+;;
+"neofetch")
+sudo rpm-ostree install neofetch
+;;
+"nl")
+sudo rpm-ostree install nl
+;;
+"all")
+sudo rpm-ostree install vim nano task podman gcc vmstat neofetch
+;;
+*)
+echo "sorry, tool name not found!"
 esac
-
