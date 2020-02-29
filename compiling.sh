f9ade6f94f82b23e70b7b12d144f8645f3062316
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-29 18:02:48 +0200

    add compiling file

---
+++ compiling.sh
@@ -0,0 +1,11 @@
+# here program need to be build using gcc and make 
+# this is program require ftp or lftp to connect to the server
+
+
+#programs is 
+
+# rcs - reversion  control system GNU RCS 
+# diction 
+
+
+
